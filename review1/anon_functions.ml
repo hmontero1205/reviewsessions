385739ecfb504de35b11b54d055f8037b28d7d49
Hans Montero <hjm2133@columbia.edu>
2021-01-26 18:01:15 -0500

    Implement get_gt for lambda example of closure

--- review1/anon_functions.ml
+++ review1/anon_functions.ml
@@ -3,3 +3,5 @@ let l = [ 0;5;1023;-8;10; ]
 let is_pos = List.map (fun x -> x > 0) l
 
 let only_pos = List.fold_left (fun a b -> a && b) true is_pos
+
+let get_gt key lst = List.fold_right (fun x l -> if x > key then x :: l else l) lst []
